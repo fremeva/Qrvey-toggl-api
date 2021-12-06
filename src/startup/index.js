d8fff68908bcda2b79ef8d369b66402e61bf170d
Fredy Mendoza Vargas <fredy.mendoza.ext@nokia.com>
2021-12-05 22:19:42 -0500

    docs(jsdoc): Generate init documentation with jsdoc to AppServer class
    init documentation generated with jsdoc. The documentation initi with Appserver class, methods, etc.

--- src/startup/index.js
+++ src/startup/index.js
@@ -1,15 +1,33 @@
 const express = require('express');
 
+/**
+ * Class represent a express server application
+ */
 class AppServer {
+  /**
+   * Create and configure express server application
+   * @param {Object} dependency Dependency injections
+   * @param {Object} dependency.Setting Setting object has all enviroments and configuration var
+   * @param {express.Router} dependency.RouterApp All routes to application
+   */
   constructor({ Setting, RouterApp }) {
     this._setting = Setting;
     this._app = express().use(RouterApp);
   }
 
+  /**
+   * Get app value
+   * @return {express.Application} The app express instance.
+   */
   get app() {
     return this._app;
   }
 
+  /**
+   * Run express server application
+   *
+   * @async
+   */
   async run() {
     this._app.listen(this._setting.PORT, (err) => {
       if (err) throw new Error(err);
