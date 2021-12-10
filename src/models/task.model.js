eed96235b5922b7dbd15c178225ccc356e412f5b
Fredy Mendoza Vargas <fredy.mendoza.ext@nokia.com>
2021-12-10 01:22:35 -0500

    refactor(models): add select field autopopulate populate and virtual pop
    Add virtual populate in task with get all tracking times data related.

--- src/models/task.model.js
+++ src/models/task.model.js
@@ -3,6 +3,10 @@
  * @module Models
  */
 const { Schema, model } = require('mongoose');
+const { STATUS_TASK } = require('./../core/constant');
+
+const STATUS_TASK_VALUES = Object.values(STATUS_TASK);
+
 /**
  * Task schema mongoose to represent document in MongoDB
  * @constructor Task
@@ -18,13 +22,22 @@ const TaskSchema = new Schema(
       type: Schema.Types.ObjectId,
       ref: 'User',
       required: [true, 'owner is required'],
-      autopopulate: true
+      autopopulate: { select: 'username name' }
     },
     project: {
       type: Schema.Types.ObjectId,
       ref: 'Project',
       required: false,
-      autopopulate: true
+      autopopulate: { select: 'name -owner' }
+    },
+    status: {
+      type: String,
+      required: [true, 'status is required'],
+      enum: {
+        values: STATUS_TASK_VALUES,
+        message: '{VALUE} is not supported'
+      },
+      default: STATUS_TASK.RUNNING
     }
   },
   {
@@ -32,9 +45,24 @@ const TaskSchema = new Schema(
       createdAt: true,
       updatedAt: true
     },
-    toObject: { getters: true }
+    toObject: { getters: true },
+    toJSON: {
+      getters: true,
+      transform: function (doc, ret) {
+        return ret;
+      }
+    }
   }
 );
+
+// Virtual Population
+TaskSchema.virtual('trackingtimes', {
+  ref: 'TrackingTimeTask',
+  localField: '_id',
+  foreignField: 'task',
+  autopopulate: { select: '-task duration' }
+});
+// TaskSchema.virtual('totaltimes', {ref: 'TrackingTimeTask', localField:'_id', foreignField:'task', count:true, autopopulate:true});
 
 // Adding Plugins
 TaskSchema.plugin(require('mongoose-autopopulate'));
